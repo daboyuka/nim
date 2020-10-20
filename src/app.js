090ecf75f7181de63b993eda53c824f971ac5714
Drew Boyuka <drew@pendo.io>
2020-10-19 21:35:02 -0400

    Babel and some code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-require('bootstrap')
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.min.css');
 
-require("./main.css")
+require("./main.css");
 
-import {html, render} from 'lit-html';
+import {render} from 'lit-html';
+import {LitElement, property, html, css, customElement} from 'lit-element';
+
+const hamburger = '🍔';
+
+@customElement('nim-counters')
+export class NimCounters extends LitElement {
+    @property({type: Array}) counts = [0];
+
+    static get styles() {
+        return css`
+            .base { display: flex; font-size: 3vw; }
+        `;
+    }
+    render() {
+        console.log(typeof this.counts)
+        return html`
+            ${this.counts.map((c, i) => html`
+                <div class="base">
+                    ${this.counts.length > 1 ? html`
+                        <div class="base" style="flex: 0 0 3em; justify-content: flex-end;">Pile ${i+1}</div>
+                    ` : null}
+                    <div class="base" style="flex: 1 0 0; justify-content: flex-end;">${hamburger.repeat(c)}</div>
+                    <div class="base" style="flex: 0 0 3em; justify-content: flex-end; align-items: center;">${c}</div>
+                </div>
+            `)}
+        `;
+    }
+}
 
 render(html`
-    <div class="container">
-    <h1>Hello world</h1>  
+    <div class="container d-flex h-100">
+    <div class="row w-100 align-self-center border" style="padding: 0.5em;"> 
+        <nim-counters counts="[3,4]" name="bob" class="w-100"/>
+    </div>
     </div>
 `, document.body)
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/app.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Nim',
-    }),
-  ],
-  module: {
-    rules:[
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-    ]
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: {
+        app: './src/app.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Nim',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
